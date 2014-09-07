66a1b1d5b02e6985f046481bbc972af915334244
Shamim Ahmed <shamim.buet.99@gmail.com>
2014-09-07 23:22:02 +1000

    Added a basic servlet to the webapp

---
+++ ocejwcd-proj01/src/main/java/edu/buet/cse/ocejwcd/proj01/HelloWorldServlet.java
@@ -0,0 +1,34 @@
+package edu.buet.cse.ocejwcd.proj01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author shamim
+ */
+@WebServlet(name = "HWServlet",
+        initParams = @WebInitParam(name = "count", value = "5"),
+        urlPatterns = "/hello.do")
+public class HelloWorldServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    response.setContentType("text/plain");
+    PrintWriter out = response.getWriter();
+    int count = Integer.parseInt(getInitParameter("count"));
+    
+    for (int i = 0; i < count; i++) {
+      out.println("hello world !!");
+    }
+    
+    out.close();
+  }
+}
---
+++ ocejwcd-proj01/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!doctype html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome </title>
+  </head>
+  <body>
+    <h1>Welcome to the world of OCEJWCD !</h1>
+    <p>Click <a href="hello.do">here</a> to invoke Hello World Servlet</p>
+  </body>
+</html>
